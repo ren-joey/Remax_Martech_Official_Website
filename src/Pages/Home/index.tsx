885eb12776eeaa76b01303b9a63193530d4acf34
ren-joey <tsuyiren@gmail.com>
2023-12-25 01:42:07 +0800

    feat: 🎸 add new bg images and fix uppercase file name (1/2)

--- src/Pages/Home/index.tsx
+++ src/Pages/Home/index.tsx
@@ -2,11 +2,15 @@ import { useEffect, useState } from 'react';
 import './home.scss';
 import ASScroll from '@ashthornton/asscroll';
 import Wrapper from '../../Components/Shared/Wrapper';
-import Section1 from '../../Components/Sections/section1';
-import Section2 from '../../Components/Sections/section2';
-import Section3 from '../../Components/Sections/section3';
-import Section4 from '../../Components/Sections/section4';
-import Section5 from '../../Components/Sections/section5';
+import Section1 from '../../Components/Sections/Section1';
+import Section2 from '../../Components/Sections/Section2';
+import Section3 from '../../Components/Sections/Section3';
+import Section4 from '../../Components/Sections/Section4';
+import Section5 from '../../Components/Sections/Section5';
+import Section1to2 from '../../Components/Sections/Section1to2';
+import Section2to3 from '../../Components/Sections/Section2to3';
+import Section3to4 from '../../Components/Sections/Section3to4';
+import Section4to5 from '../../Components/Sections/Section4to5';
 
 const Home = () => {
     const [asscroll, setAsscroll] = useState<ASScroll|null>(null);
@@ -32,9 +36,13 @@ const Home = () => {
             >
                 <div>
                     <Section1 />
+                    <Section1to2 />
                     <Section2 />
+                    <Section2to3 />
                     <Section3 />
+                    <Section3to4 />
                     <Section4 />
+                    <Section4to5 />
                     <Section5 />
                 </div>
             </div>
