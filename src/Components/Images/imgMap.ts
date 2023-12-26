6d480c171f14bc4dbbaee605e2755fdce98034a2
ren-joey <tsuyiren@gmail.com>
2023-12-26 11:48:03 +0800

    asset: 🖼️ add new images

--- src/Components/Images/imgMap.ts
+++ src/Components/Images/imgMap.ts
@@ -3,16 +3,20 @@ import section_7_service_1_img_2 from '../../assets/images/photo/section_7_servi
 import section_7_service_1_img_3 from '../../assets/images/photo/section_7_service_1_img_3.jpg';
 import section_7_service_1_img_4 from '../../assets/images/photo/section_7_service_1_img_4.jpg';
 import section_7_service_2_img_1 from '../../assets/images/photo/section_7_service_2_img_1.jpg';
+import section_7_service_2_img_2 from '../../assets/images/photo/section_7_service_2_img_2.jpg';
+import section_7_service_2_img_3 from '../../assets/images/photo/section_7_service_2_img_3.jpg';
 import section_7_service_3_img_1 from '../../assets/images/photo/section_7_service_3_img_1.jpg';
 import section_7_service_3_img_2 from '../../assets/images/photo/section_7_service_3_img_2.jpg';
 import section_7_service_3_img_3 from '../../assets/images/photo/section_7_service_3_img_3.jpg';
 import section_7_service_4_img_1 from '../../assets/images/photo/section_7_service_4_img_1.jpg';
 import section_7_service_4_img_2 from '../../assets/images/photo/section_7_service_4_img_2.jpg';
+import section_7_service_4_img_3 from '../../assets/images/photo/section_7_service_4_img_3.jpg';
 import section_7_service_5_img_1 from '../../assets/images/photo/section_7_service_5_img_1.jpg';
 import section_7_service_5_img_2 from '../../assets/images/photo/section_7_service_5_img_2.jpg';
 import section_7_service_6_img_1 from '../../assets/images/photo/section_7_service_6_img_1.jpg';
 import section_7_service_6_img_2 from '../../assets/images/photo/section_7_service_6_img_2.jpg';
 import section_7_service_6_img_3 from '../../assets/images/photo/section_7_service_6_img_3.jpg';
+import section_7_service_7_img_1 from '../../assets/images/photo/section_7_service_7_img_1.jpg';
 import section_7_service_7_img_2 from '../../assets/images/photo/section_7_service_7_img_2.jpg';
 import section_7_service_7_img_3 from '../../assets/images/photo/section_7_service_7_img_3.jpg';
 import section_7_service_7_img_4 from '../../assets/images/photo/section_7_service_7_img_4.jpg';
@@ -31,6 +35,15 @@ import about_us_img_1 from '../../assets/images/photo/about-us-img-1.png';
 // import service_banner from '../../assets/images/photo/service-banner.png';
 // import service_bg from '../../assets/images/bg/service-bg.png';
 
+import section_5_case_1 from '../../assets/images/photo/section_5_case_1.jpg';
+import section_5_case_2 from '../../assets/images/photo/section_5_case_2.jpg';
+import section_5_case_3 from '../../assets/images/photo/section_5_case_3.jpg';
+import section_5_case_4 from '../../assets/images/photo/section_5_case_4.jpg';
+import section_5_case_5 from '../../assets/images/photo/section_5_case_5.jpg';
+import section_5_case_6 from '../../assets/images/photo/section_5_case_6.jpg';
+import section_5_case_7 from '../../assets/images/photo/section_5_case_7.jpg';
+import section_5_case_8 from '../../assets/images/photo/section_5_case_8.jpg';
+
 
 const ImgMap = {
     section_7_service_1_img_1,
@@ -38,16 +51,20 @@ const ImgMap = {
     section_7_service_1_img_3,
     section_7_service_1_img_4,
     section_7_service_2_img_1,
+    section_7_service_2_img_2,
+    section_7_service_2_img_3,
     section_7_service_3_img_1,
     section_7_service_3_img_2,
     section_7_service_3_img_3,
     section_7_service_4_img_1,
     section_7_service_4_img_2,
+    section_7_service_4_img_3,
     section_7_service_5_img_1,
     section_7_service_5_img_2,
     section_7_service_6_img_1,
     section_7_service_6_img_2,
     section_7_service_6_img_3,
+    section_7_service_7_img_1,
     section_7_service_7_img_2,
     section_7_service_7_img_3,
     section_7_service_7_img_4,
@@ -61,10 +78,18 @@ const ImgMap = {
     section_7_service_9_img_6,
     section_7_service_9_img_7,
     // about_us_banner,
-    about_us_img_1
+    about_us_img_1,
     // about_us_bg,
     // service_banner,
-    // service_bg
+    // service_bg,
+    section_5_case_1,
+    section_5_case_2,
+    section_5_case_3,
+    section_5_case_4,
+    section_5_case_5,
+    section_5_case_6,
+    section_5_case_7,
+    section_5_case_8
 };
 
 export default ImgMap;
